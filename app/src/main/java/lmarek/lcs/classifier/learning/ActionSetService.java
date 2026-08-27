c2f9b533c011bb32ff840ef27354592cbd4ee438
Łukasz Marek <marek.lucas@gmail.com>
2026-08-27 16:48:50 +0200

    make ActionSetService pkg private

--- app/src/main/java/lmarek/lcs/classifier/learning/ActionSetService.java
+++ app/src/main/java/lmarek/lcs/classifier/learning/ActionSetService.java
@@ -5,7 +5,7 @@ import lmarek.lcs.classifier.rule.Action;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ActionSetService {
+class ActionSetService {
   ActionSet selectAction(Action action, MatchSet matchSet) {
     var classifiers = matchSet.classifiersByPrediction().getOrDefault(action, List.of());
     return new ActionSetBuilder().action(action).classifiers(classifiers).build();
