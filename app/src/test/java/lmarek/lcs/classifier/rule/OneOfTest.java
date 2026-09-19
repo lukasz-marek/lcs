ce7971fbdf23b281cf49fccbf71e3172033ee692
Łukasz Marek <marek.lucas@gmail.com>
2026-09-19 18:08:33 +0200

    improve tests in OneOfTest

--- app/src/test/java/lmarek/lcs/classifier/rule/OneOfTest.java
+++ app/src/test/java/lmarek/lcs/classifier/rule/OneOfTest.java
@@ -1,7 +1,10 @@
 package lmarek.lcs.classifier.rule;
 
+import java.util.HashSet;
+import java.util.Set;
 import lmarek.lcs.classifier.symbol.Symbol;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -25,6 +28,62 @@ class OneOfTest {
     var sut = new OneOf(Symbol.of("black"), Symbol.of("white"));
     // when
     var matches = sut.matches(Symbol.of(tested));
+    // then
+    Assertions.assertThat(matches).isFalse();
+  }
+
+  @Test
+  void shouldKeepMatchingOriginalSymbolsWhenSourceSetChanges() {
+    // given
+    var red = Symbol.of("red");
+    var green = Symbol.of("green");
+    var allowedValues = new HashSet<>(Set.of(red));
+    var sut = new OneOf(allowedValues);
+
+    // when
+    allowedValues.clear();
+    allowedValues.add(green);
+
+    // then
+    Assertions.assertThat(sut.matches(red)).isTrue();
+    Assertions.assertThat(sut.matches(green)).isFalse();
+  }
+
+  @Test
+  void shouldKeepMatchingOriginalSymbolsWhenSourceArrayChanges() {
+    // given
+    var red = Symbol.of("red");
+    var green = Symbol.of("green");
+    var allowedValues = new Symbol[] {red};
+    var sut = new OneOf(allowedValues);
+
+    // when
+    allowedValues[0] = green;
+
+    // then
+    Assertions.assertThat(sut.matches(red)).isTrue();
+    Assertions.assertThat(sut.matches(green)).isFalse();
+  }
+
+  @Test
+  void shouldNotAllowModifyingAllowedValues() {
+    // given
+    var allowedValues = new HashSet<>(Set.of(Symbol.of("red")));
+    var sut = new OneOf(allowedValues);
+
+    // when / then
+    Assertions.assertThatThrownBy(() -> sut.allowedValues().clear())
+        .isInstanceOf(UnsupportedOperationException.class);
+  }
+
+  @Test
+  void shouldNotMatchWhenAllowedValuesAreEmpty() {
+    // given
+    var sut = new OneOf();
+
+    // when
+    var matches = sut.matches(Symbol.of("red"));
+
     // then
     Assertions.assertThat(matches).isFalse();
   }
